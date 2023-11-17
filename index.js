d7f2b11faed50154f4eb3ed1571c8afcb47111ca
AnthonyIrudhayaAakash <anthonyaakash41@gmail.com>
2023-11-17 14:44:13 +0530

    EP-1

---
+++ index.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{id:"heading_hello"},"Hello World");
+
+const container = ReactDOM.createRoot(document.getElementById("container"));
+
+container.render(heading);
